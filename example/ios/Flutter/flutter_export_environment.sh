521796075faebbb06958a7383f9f0ec4746d0b34
Cristiano Cantelli <cristiano.cantelli@advinser.it>
2025-09-16 12:33:06 +0200

    chore: changed intl dependency

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Flutter\sqfEntity\example"
+export "FLUTTER_ROOT=C:\Users\CristianoCantelli\FlutterSdk\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\CristianoCantelli\AndroidStudioProjetcs\sqfEntity\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
